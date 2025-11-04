b0812f62df2028ef7cd8d893fba704f1f23bab8e
Alejandro Parra <alejandroparral@gmail.com>
2025-11-03 23:34:45 -0500

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // These URLs are placeholders for development and should be replaced with actual production URLs.
-  API_BASE_URL: 'https://831r3k0v28.execute-api.us-east-1.amazonaws.com/dev/',
-  STATIC_ASSETS_URL: 'https://d1iglpdyw6wqcz.cloudfront.net/'
+  API_BASE_URL: 'https://831r3k0v28.execute-api.us-east-1.amazonaws.com/dev',
+  STATIC_ASSETS_URL: 'https://d1iglpdyw6wqcz.cloudfront.net'
 };
