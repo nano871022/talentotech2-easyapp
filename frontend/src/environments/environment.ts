af318c88d4467b6225efd069485ac85039ac9d8e
Alejandro Parra <alejandroparral@gmail.com>
2025-11-03 23:27:06 -0500

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  API_BASE_URL: 'http://localhost:8080/api/v1',
-  STATIC_ASSETS_URL: 'http://localhost:9080/'
+  API_BASE_URL: 'https://831r3k0v28.execute-api.us-east-1.amazonaws.com/dev/',
+  STATIC_ASSETS_URL: 'https://d1iglpdyw6wqcz.cloudfront.net/'
 };
