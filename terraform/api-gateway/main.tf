5d13343aab9904c87a3b1c90d340d8d034e2159a
Yeison Liscano <yliscanoc@unal.edu.co>
2025-11-04 09:01:39 -0500

    use apigateway version 2

--- terraform/api-gateway/main.tf
+++ terraform/api-gateway/main.tf
@@ -1,51 +1,78 @@
-resource "aws_api_gateway_rest_api" "main" {
-  body = jsonencode({
-    openapi = "3.0.1"
-    info = {
-      title   = "${var.project_name}-api"
-      version = "1.0"
-    }
-    paths = {
-      "/auth" = {
-        x-amazon-apigateway-any-method = {
-          x-amazon-apigateway-integration = {
-            httpMethod           = "POST"
-            type                 = "AWS_PROXY"
-            uri                  = var.lambda_auth_invoke_arn
-          }
-        }
-      }
-      "/api" = {
-        x-amazon-apigateway-any-method = {
-          x-amazon-apigateway-integration = {
-            httpMethod           = "POST"
-            type                 = "AWS_PROXY"
-            uri                  = var.lambda_advise_invoke_arn
-          }
-        }
-      }
-    }
-  })
-
-  name = "${var.project_name}-api"
+# API Gateway v2 (HTTP API)
+resource "aws_apigatewayv2_api" "main" {
+  name          = "${var.project_name}-api"
+  protocol_type = "HTTP"
 }
 
-resource "aws_api_gateway_deployment" "main" {
-  rest_api_id = aws_api_gateway_rest_api.main.id
+# Lambda Integration para Auth Service
+resource "aws_apigatewayv2_integration" "auth" {
+  api_id           = aws_apigatewayv2_api.main.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = var.lambda_auth_invoke_arn
 
-  triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.main.body))
-  }
+  payload_format_version = "2.0"
+}
 
-  lifecycle {
-    create_before_destroy = true
+# Lambda Integration para Advise Service
+resource "aws_apigatewayv2_integration" "advise" {
+  api_id           = aws_apigatewayv2_api.main.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = var.lambda_advise_invoke_arn
+
+  payload_format_version = "2.0"
+}
+
+# Routes para Auth Service
+resource "aws_apigatewayv2_route" "auth" {
+  api_id    = aws_apigatewayv2_api.main.id
+  route_key = "ANY /auth/{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.auth.id}"
+}
+
+resource "aws_apigatewayv2_route" "auth_root" {
+  api_id    = aws_apigatewayv2_api.main.id
+  route_key = "ANY /auth"
+  target    = "integrations/${aws_apigatewayv2_integration.auth.id}"
+}
+
+# Routes para Advise Service
+resource "aws_apigatewayv2_route" "advise" {
+  api_id    = aws_apigatewayv2_api.main.id
+  route_key = "ANY /api/{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.advise.id}"
+}
+
+resource "aws_apigatewayv2_route" "advise_root" {
+  api_id    = aws_apigatewayv2_api.main.id
+  route_key = "ANY /api"
+  target    = "integrations/${aws_apigatewayv2_integration.advise.id}"
+}
+
+# Stage con auto-deploy
+resource "aws_apigatewayv2_stage" "main" {
+  api_id      = aws_apigatewayv2_api.main.id
+  name        = var.environment
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gateway.arn
+    format = jsonencode({
+      requestId      = "$context.requestId"
+      ip             = "$context.identity.sourceIp"
+      requestTime    = "$context.requestTime"
+      httpMethod     = "$context.httpMethod"
+      routeKey       = "$context.routeKey"
+      status         = "$context.status"
+      protocol       = "$context.protocol"
+      responseLength = "$context.responseLength"
+    })
   }
 }
 
-resource "aws_api_gateway_stage" "main" {
-  deployment_id = aws_api_gateway_deployment.main.id
-  rest_api_id   = aws_api_gateway_rest_api.main.id
-  stage_name    = var.environment
+# CloudWatch Log Group para API Gateway
+resource "aws_cloudwatch_log_group" "api_gateway" {
+  name              = "/aws/apigateway/${var.project_name}-api"
+  retention_in_days = 7
 }
 
 # Permisos para que API Gateway invoque las Lambdas
@@ -54,7 +81,7 @@ resource "aws_lambda_permission" "auth" {
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_auth_function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.main.execution_arn}/*/*"
+  source_arn    = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
 }
 
 resource "aws_lambda_permission" "advise" {
@@ -62,5 +89,5 @@ resource "aws_lambda_permission" "advise" {
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_advise_function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.main.execution_arn}/*/*"
+  source_arn    = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
 }
--- terraform/api-gateway/outputs.tf
+++ terraform/api-gateway/outputs.tf
@@ -1,3 +1,14 @@
 output "api_gateway_url" {
-  value = aws_api_gateway_deployment.main.invoke_url
+  value       = aws_apigatewayv2_stage.main.invoke_url
+  description = "API Gateway v2 invoke URL"
+}
+
+output "api_gateway_id" {
+  value       = aws_apigatewayv2_api.main.id
+  description = "API Gateway v2 ID"
+}
+
+output "api_gateway_endpoint" {
+  value       = aws_apigatewayv2_api.main.api_endpoint
+  description = "API Gateway v2 endpoint"
 }
